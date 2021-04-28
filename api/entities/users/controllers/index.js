acf8de3a4c56e67f2012c770bd8871270d75ca2e
Daniel <danihtoledo22@gmail.com>
2021-04-28 20:48:32 -0300

    rotas de crud de users (ainda n 100%)

--- api/entities/users/controllers/index.js
+++ api/entities/users/controllers/index.js
@@ -1,3 +1,38 @@
 const router = require('express').Router();
+const UserService = require('../services/UserService');
+
+router.post('/', (req, res) => {
+    try {
+        UserService.createUser(req.body.user);
+        res.sendStatus(200);
+    } catch {
+        (e) => console.log(e);
+    }
+});
+
+router.get('/', (req, res) => {
+    try {
+        UserService.getUser(req.body.id);
+        res.send(200);
+    } catch {
+        (e) => console.log(e);
+    }
+});
+
+router.put('/', (req, res) => {
+    try {
+        UserService.alterUser(req.body.id, req.body.user);
+    } catch {
+        (e) => console.log(e);
+    }
+});
+
+router.delete('/', (req, res) => {
+    try {
+        UserService.deleteUser(req.body.id);
+    } catch {
+        (e) => console.log(e);
+    }
+});
 
 module.exports = router;
