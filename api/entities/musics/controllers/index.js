d9405c84ad800acbe4d2bea242d4b0ed8cb221e4
Daniel <danihtoledo22@gmail.com>
2021-05-26 16:53:33 -0300

    herança de gênero e author id pra musica

--- api/entities/musics/controllers/index.js
+++ api/entities/musics/controllers/index.js
@@ -1,97 +1,100 @@
-const router = require('express').Router();
-const AlbumService = require('../../albums/services/AlbumService');
-const MusicService = require('../services/MusicService');
+const NotAuthorizedError = require('../../../errors/NotAuthorizedError');
+const InvalidParamError = require('../../../errors/InvalidParamError');
 const CustomerMusicService = require(
   '../../users/services/CustomerMusicService');
-const InvalidParamError = require('../../../errors/InvalidParamError');
+const AlbumService = require('../../albums/services/AlbumService');
+const MusicService = require('../services/MusicService');
 const {
   jwtMiddleware,
-  checkRole} = require('../../../middlewares/auth-middlewares');
+  checkRole,
+} = require('../../../middlewares/auth-middlewares');
+const router = require('express').Router();
 
 router.use(jwtMiddleware);
 
 // Aceita uma ou várias músicas e as registra no album com o id passado no
 // params
-router.post('/:id',
-  checkRole(['artist']),
-  async (req, res, next) => {
-    try {
-      const albumId = req.params.id;
-      const {musics} = req.body;
-      if (musics.length > 0) {
-        const album = await AlbumService.getAlbumById(albumId);
-        for (music of musics) {
-          console.log(music);
-          music.image = album.image;
-          await album.createMusic(music);
-        }
-        res.status(201).json(musics);
-      } else {
-        throw new InvalidParamError(
-          'É necessário pelo menos uma música a ser criada ' +
-            'para a utilização dessa rota');
+router.post('/:id', checkRole(['artist']), async (req, res, next) => {
+  try {
+    const albumId = req.params.id;
+    const {musics} = req.body;
+    if (musics.length > 0) {
+      const album = await AlbumService.getAlbumById(albumId);
+      for (music of musics) {
+        console.log(music);
+        music.image = album.image;
+        music.authorId = album.authorId;
+        music.genre = music.genre ? music.genre : album.genre;
+        await album.createMusic(music);
       }
-    } catch (error) {
-      next(error);
+      res.status(201).json(musics);
+    } else {
+      throw new InvalidParamError(
+        'É necessário pelo menos uma música a ser criada ' +
+          'para a utilização dessa rota',
+      );
     }
-  },
-);
+  } catch (error) {
+    next(error);
+  }
+});
 
 // Comprar música
-router.post('/store/:id',
-  async (req, res, next) => {
-    try {
-      const userId = req.user.id;
-      const musicId = req.params.id;
-      CustomerMusicService.buyMusic(userId, musicId);
-      res.status(200).json('Compra realizada!');
-    } catch (error) {
-      next(error);
-    }
-  },
-);
+router.post('/store/:id', async (req, res, next) => {
+  try {
+    const userId = req.user.id;
+    const musicId = req.params.id;
+    CustomerMusicService.buyMusic(userId, musicId);
+    res.status(200).json('Compra realizada!');
+  } catch (error) {
+    next(error);
+  }
+});
 
-// Pegar música especifica pelo id
-router.get('/:id',
-  async (req, res, next) => {
-    try {
-      const music = await MusicService.getMusicById(req.params.id);
-      res.status(200).json(music);
-    } catch (error) {
-      next(error);
+// Requisição para escutar música
+router.get('/listen/:id', async (req, res, next) => {
+  try {
+    const userId = req.user.id;
+    const musicId = req.params.id;
+    const haveMusic = await CustomerMusicService.checkHaveMusic(
+      userId,
+      musicId,
+    );
+    if (!haveMusic) {
+      throw new NotAuthorizedError('Você não possui essa música');
     }
-  },
-);
+    const music = await MusicService.getMusicById(musicId);
+    res.status(200).json(music);
+  } catch (error) {
+    next(error);
+  }
+});
 
 // Get all musics (n creio que vá ser utilizado)
-router.get('/',
-  async (req, res, next) => {
-    try {
-      const musics = await MusicService.getAllMusics();
-      res.status(200).json(musics);
-    } catch (error) {
-      next(error);
-    }
-  },
-);
+router.get('/', async (req, res, next) => {
+  try {
+    const musics = await MusicService.getAllMusics();
+    res.status(200).json(musics);
+  } catch (error) {
+    next(error);
+  }
+});
 
 // Edit música específica
-router.patch('/:id',
-  checkRole(['artist']),
-  async (req, res, next) => {
-    try {
-      const body = req.body;
-      const musicId = req.params.id;
-      await MusicService.updateMusicInfo(musicId, body);
-      res.status(200).json({...body, ...{id: musicId}});
-    } catch (error) {
-      next(error);
-    }
-  },
-);
+router.patch('/:id', checkRole(['artist']), async (req, res, next) => {
+  try {
+    const body = req.body;
+    const musicId = req.params.id;
+    await MusicService.updateMusicInfo(musicId, body);
+    res.status(200).json({...body, ...{id: musicId}});
+  } catch (error) {
+    next(error);
+  }
+});
 
 // Delete música específica
-router.delete('/:id',
+router.delete(
+  '/:id',
   checkRole(['artist', 'admin']),
   async (req, res, next) => {
     try {
--- api/entities/users/services/CustomerMusicService.js
+++ api/entities/users/services/CustomerMusicService.js
@@ -1,32 +1,40 @@
 const {CustomerMusic} = require('../../../database/initializer');
 const MusicService = require('../../musics/services/MusicService');
+const AlbumService = require('../../albums/services/AlbumService');
+
 class CustomerMusicService {
   async getMusics(userId) {
-    try {
-      const userMusicsRelations = await CustomerMusic.findAll({userId: userId});
-      const userMusics = [];
-      for (const musicRelation of userMusicsRelations) {
-        const musicId = musicRelation.musicId;
-        const music = await MusicService.getMusicById(musicId);
-        userMusics.push(music);
-      }
-      return userMusics;
-    } catch (error) {
-      throw error;
+    const userMusicsRelations = await CustomerMusic.findAll(
+      {where: {userId: userId}},
+    );
+    const userMusics = [];
+    for (const musicRelation of userMusicsRelations) {
+      const musicId = musicRelation.musicId;
+      const music = await MusicService.getMusicById(musicId);
+      userMusics.push(music);
     }
+    return userMusics;
   }
 
-  async checkHaveMusic(userId, musicId) {}
+  async checkHaveMusic(userId, musicId) {
+    const haveMusic = await CustomerMusic.findOne({
+      where: {userId: userId, musicId: musicId},
+    });
+    if (haveMusic) return true;
+    return false;
+  }
 
   async buyMusic(userId, musicId) {
-    try {
-      await CustomerMusic.create({userId, musicId});
-    } catch (error) {
-      throw error;
-    }
+    await CustomerMusic.create({userId, musicId});
   }
 
-  async buyAlbum(albumID) {}
+  async buyAlbum(userId, albumID) {
+    const album = await AlbumService.getAlbumById(albumID);
+    const albumMusics = await album.getMusics();
+    for (const music of albumMusics) {
+      this.buyMusic(userId, music.id);
+    }
+  }
 }
 
 module.exports = new CustomerMusicService();
